cd8f2fd5868654b86c900dbe191657516504dab0
Tyler Forbes <tyler.forbes@cognizant.com>
2023-03-08 05:56:54 -0600

    calls server to retrieve rss items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
+getRss();
+
+const fallBack = <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                     Site under construction
+                 </header>
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {fallBack}
     </div>
   );
+}
+
+async function getRss() {
+try {
+    const serverUrl = "http://localhost:8080/rssFeed";
+    let rssGet = await axios.get(serverUrl, {
+    headers: { 'Access-Control-Allow-Origin' : 'http://localhost:8080',
+                'Access-Control-Allow-Methods':'GET',
+//                "Content-Type": "application/json",
+                },
+    responseType: "json"});
+
+    console.log(rssGet.data)
+    } catch(e) {
+    console.log(e)}
 }
 
 export default App;
